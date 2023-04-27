1094dca8f6e52401496db7a6e34caa7b0c06fb59
LucasChoi <vv@gmail.com>
2023-04-28 05:45:29 +0800

    no message

--- saleor/order/notifications.py
+++ saleor/order/notifications.py
@@ -148,6 +148,12 @@ def get_lines_payload(order_lines: Iterable["OrderLine"]):
     return payload
 
 
+def get_fulfillments_lines_payload(lines: Iterable["FulfillmentLine"]):
+    payload = []
+    for line in lines:
+        payload.append(get_default_fulfillment_line_payload(line))
+    return payload
+
 ADDRESS_MODEL_FIELDS = [
     "first_name",
     "last_name",
@@ -243,7 +249,7 @@ def get_default_order_payload(order: "Order", redirect_url: str = ""):
         "variant__product__attributes__assignment__attribute",
         "variant__product__attributes__values",
     ).all()
-    fulfillments_lines = list(order.fulfillments.all())
+    fulfillments_lines = order.fulfillments.all()
     currency = order.currency
     quantize_price_fields(order, fields=ORDER_PRICE_FIELDS, currency=currency)
     order_payload = model_to_dict(order, fields=ORDER_MODEL_FIELDS)
@@ -262,7 +268,7 @@ def get_default_order_payload(order: "Order", redirect_url: str = ""):
             "subtotal_net_amount": quantize_price(subtotal.net.amount, currency),
             "tax_amount": quantize_price(tax, currency),
             "lines": get_lines_payload(lines),
-            "fulfillments": [get_default_fulfillment_line_payload(line) for line in fulfillments_lines.lines],
+            "fulfillments": get_fulfillments_lines_payload(fulfillments_lines),
             "billing_address": get_address_payload(order.billing_address),
             "shipping_address": get_address_payload(order.shipping_address),
             "shipping_method_name": order.shipping_method_name,
